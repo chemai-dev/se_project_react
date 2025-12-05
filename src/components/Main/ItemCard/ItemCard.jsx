e8a9a8a6838cb3bde07d408d069351e088ae3217
chemai.div <chemai.dev@gmail.com>
2025-12-05 15:21:10 -0500

    Feat: Add initial main section

--- src/components/Main/ItemCard/ItemCard.jsx
+++
@@ -1,7 +0,0 @@
-import "./ItemCard.css";
-
-function ItemCard() {
-  return <div className="item-card">{/* Weather card content here */}</div>;
-}
-
-export default ItemCard;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,15 +1,23 @@
 import "./Main.css";
-import WeatherCard from "./WeatherCard/WeatherCard";
-import ItemCard from "./ItemCard/ItemCard";
+import WeatherCard from "../App/WeatherCard/WeatherCard";
+import ItemCard from "../App/ItemCard/ItemCard";
+import { defaultClothingItems } from "../../utils/constants";
 
-function Main() {
+function Main({ weatherData }) {
   return (
     <main className="main">
       <WeatherCard />
       <section className="cards">
         <p className="cards__text">Today is 75° F / You may want to wear:</p>
-
-        <ItemCard />
+        <ul className="cards__list">
+          {defaultClothingItems
+            .filter((item) => {
+              return item.weather === weatherData.type;
+            })
+            .map((item) => {
+              return <ItemCard key={item._id} item={item} />;
+            })}
+        </ul>
       </section>
     </main>
   );
